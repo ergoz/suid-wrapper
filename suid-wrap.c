c87f62e942ad2bfa8b54e10344a4d2d36a373143
Rik van der Heijden <info@rikict.com>
2013-04-19 22:02:35 +0200

    Run checkpatch on suid-wrap.c

--- suid-wrap.c
+++ suid-wrap.c
@@ -1,26 +1,25 @@
-#include <stdio.h>;
-#include <string.h>;
+#include <stdio.h>
+#include <string.h>
 
-int main (int argc, char *argv[]) {
-  char str[1024];
-  int i;
+int main(int argc, char *argv[])
+{
+	char str[1024];
+	int i;
 
-  if (argc < 2) {
-    return 1;
-  }
+	if (argc < 2)
+		return 1;
 
-  for (i = 1; i < argc; i++) {
-    if (i == 1) {
-      strcpy(str, argv[i]);
-    } else {
-      strcat(str, argv[i]);
-    }
-    if (i < argc-1) {
-      strcat(str, " ");
-    }
-  }
+	for (i = 1; i < argc; i++) {
+		if (i == 1)
+			strcpy(str, argv[i]);
+		else
+			strcat(str, argv[i]);
 
-  setuid(0);
-  return system(str);
+		if (i < argc-1)
+			strcat(str, " ");
+	}
+
+	setuid(0);
+	return system(str);
 }
 
